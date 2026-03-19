0f12d24b296a845795845f925f8a7443e89807fc
SupStapG <kira.iovenko@gmail.com>
2026-03-18 22:11:42 -0400

    Make banner ttile and subtitle draggable and add download button functionality

--- js/banner.js
+++ js/banner.js
@@ -7,6 +7,12 @@ const bannerSubtitle = document.getElementById("bannerSubtitle");
 const bannerTitleColor = document.getElementById("bannerTitleColor");
 const bannerSubtitleColor = document.getElementById("bannerSubtitleColor");
 const bannerSubtitleSize = document.getElementById("bannerSubtitleSize");
+const bannerDownloadBtn = document.getElementById("downloadBannerBtn");
+const banner = document.getElementById("banner");
+
+let offsetX = 0;
+let offsetY = 0;
+let currentlyMoving = null;
 
 bannerTitleInput.addEventListener("input", ()=> {
     bannerTitle.textContent = bannerTitleInput.value || "Title";
@@ -28,4 +34,42 @@ bannerSubtitleColor.addEventListener("input", () => {
 });
 bannerSubtitleSize.addEventListener("input", () => {
     bannerSubtitle.style.fontSize = bannerSubtitleSize.value + "px";
+});
+
+bannerDownloadBtn.addEventListener("click", () => {
+    html2canvas(document.getElementById("banner")).then(canvas => {
+        const link = document.createElement("a");
+        link.download = "banner.png"
+        link.href=canvas.toDataURL();
+        link.click();
+    });
+});
+
+function makeDraggable(text){
+    text.addEventListener("mousedown", (e) => {
+        currentlyMoving = text;
+        const box = text.getBoundingClientRect();
+        offsetX = e.clientX - box.left;
+        offsetY = e.clientY - box.top;
+        text.style.cursor = "grabbing";
+    });
+}
+
+makeDraggable(bannerTitle);
+makeDraggable(bannerSubtitle);
+
+document.addEventListener("mouseup", () => {
+    if(currentlyMoving){
+        currentlyMoving.style.cursor = "grab";
+    }
+    currentlyMoving = null;
+});
+
+document.addEventListener("mousemove", (e) => {
+    if (!currentlyMoving) return;
+    const bannerBox = banner.getBoundingClientRect();
+    let x2 = e.clientX - bannerBox.left - offsetX;
+    let y2 = e.clientY - bannerBox.top - offsetY;
+    currentlyMoving.style.left = x2+"px";
+    currentlyMoving.style.top = y2+"px";
 });
