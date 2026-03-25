f2b072b0bfc218c320502062ac8c48bad47df396
SupStapG <kira.iovenko@gmail.com>
2026-03-25 17:40:26 -0400

    Fix UI spacing and dynamically inject Queenie assistant by moving Queenie HTML creation from static index.html into JS

--- js/queenie.js
+++ js/queenie.js
@@ -1,20 +1,49 @@
-const queenie = document.getElementById("queenie_sprite");
-const guideText = document.getElementById("guideText");
-const talking = "images/queenie_talking.png";
-const neutral = "images/queenie_neutral.png";
-const textbox = document.querySelector(".textbox");
-const blinking = "images/queenie_blinking.png";
+document.addEventListener("DOMContentLoaded", () => {
+    createQueenie();
+    queenieInitialize();
+});
+const isInTools = window.location.pathname.includes("tools");
+const basePath = isInTools ?"../":"";
+const talking = basePath + "images/queenie_talking.png";
+const neutral = basePath + "images/queenie_neutral.png";
+const blinking = basePath+ "images/queenie_blinking.png";
 
 let talkingInterval;
 let typingInterval;
 let dialogues = [];
 let currentDialogue = 0;
 let typing = false;
+let queenie;
+let guideText;
+let textbox;
+
 function talk() {
     talkingInterval = setInterval(() => {
         queenie.src = queenie.src.includes("neutral")?talking: neutral;
     }, 140);
 }
+
+function createQueenie(){
+    const queenieHtml = `
+        <div class="queenie">
+            <div class="textbox">
+                <p id="guideText"></p>
+            </div>
+            <img id="queenie_sprite" src="${neutral}" alt="Queenie sprite">
+        </div>
+    `;
+    document.body.insertAdjacentHTML("beforeend", queenieHtml);
+    queenie = document.getElementById("queenie_sprite");
+    guideText = document.getElementById("guideText");
+    textbox = document.querySelector(".textbox");
+}
+
+function queenieInitialize(){
+    dialogueSetup();
+    clickSetup();
+    blinkRandomly();
+}
+
 function stopTalking() {
     clearInterval(talkingInterval);
     talkingInterval = null;
@@ -26,11 +55,11 @@ function textAppear(text, callback) {
     guideText.textContent = "";
     typing = true;
     talk();
-    const interval = setInterval(() => {
+    typingInterval = setInterval(() => {
         guideText.textContent += text[i];
         i++;
         if (i>=text.length){
-            clearInterval(interval);
+            clearInterval(typingInterval);
             stopTalking();
             typing = false;
             if(callback) callback();
@@ -47,33 +76,6 @@ function nextDialogue(){
         }
     }
 }
-
-function userFirstVisit() {
-    dialogues = ["Hi! Welcome to KITools.", "Feel free to check all the tools in the sidebar!"]; 
-    localStorage.setItem("visitedBefore", "true");
-    nextDialogue();
-}
-
-function returnVisit(){
-    dialogues = ["Welcome back! :D", "What shall we do today?"];
-    nextDialogue();
-}
-if(!localStorage.getItem("visitedBefore")){
-    userFirstVisit();
-} else{
-    returnVisit();
-}
-
-document.querySelector(".textbox").addEventListener("click", () => {
-    if(typing){
-        clearInterval(typingInterval);
-        guideText.textContent = dialogues[currentDialogue -1];
-        stopTalking();
-        typing = false;
-    } else{
-        nextDialogue();
-    }
-});
 
 function blinkRandomly(){
     function blinkCycle(){
@@ -92,4 +94,54 @@ function blinkRandomly(){
     }
     blinkCycle();
 }
-blinkRandomly();
+function dialogueSetup(){
+    const currentPath = window.location.pathname;
+    if(currentPath.includes("whitespace")){
+        dialogues = ["Clean up time? Yes! Make the text ~bonito~", "Those sneaky double spaces... make text not neat!", "Can you  spot a double space?"];
+    }
+    else if(currentPath.includes("preview-html")){
+        dialogues = ["Preview is a good way to check for code correctness!", "Ooh, got some code to show off? :)"];
+    }
+    else if(currentPath.includes("json-formatter")){
+        dialogues = ["JSON: a beautiful format of simplicity and flexibility!", "Want to check your JSON's validity? We can also make it pretty!"];
+    }
+    else if(currentPath.includes("timer")){
+        dialogues = ["Time is precious...that's why we gotta track it!", "I wonder if anyone ever tried using a sundial as a timer..."];
+    }
+    else if(currentPath.includes("pomodoro")){
+        dialogues = ["Alright, time to focus!", "Ahem, don't get distracted... I'm watching! O_O"];
+    }
+    else if(currentPath.includes("coin")){
+        dialogues = ["I choose heads!", "I choose tails!", "Uhh...two options? That's a tough one!", "I'm not feeling lucky today... why don't you flip it for me?"];
+    }
+    else if(currentPath.includes("flashcards")){
+        dialogues = ["Study time! I can make you some company :)", "You better not skip out on these..."];
+    }
+    else if(currentPath.includes("qrCode")){
+        dialogues = ["QR codes? I know you can make it look cool!", "Let's make some fancy QR codes~"];
+    }
+    else if(currentPath.includes("banner")){
+        dialogues = ["You gotta make it memorable! Or not...", "Hmm...wanna make a banner for GitHub? Or a band? Or for me? :D"];
+    }
+    else{
+        if(!localStorage.getItem("visitedBefore")){
+            dialogues = ["Hi! Welcome to KITools.", "Pick something from the tools in the sidebar!"];
+            localStorage.setItem("visitedBefore", "true");
+        } else{
+            dialogues = ["Welcome back! :D", "What shall we do today?"];
+        }
+    }
+    nextDialogue();
+} 
+function clickSetup(){
+    textbox.addEventListener("click", () =>{
+        if(typing){
+            clearInterval(typingInterval);
+            guideText.textContent = dialogues[Math.max(0,currentDialogue -1)];
+            stopTalking();
+            typing=false;
+        } else{
+            nextDialogue();
+        }
+    });
+}
